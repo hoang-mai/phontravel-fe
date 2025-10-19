c1fb3ba0395639dadb764b503a5114cf76b08016
HoangMA <maianhhoang31072003@gmail.com>
2025-10-19 14:11:28 +0700

    Update role and name

--- src/untils/constant.ts
+++ src/untils/constant.ts
@@ -5,10 +5,10 @@ export const members: Member[] = [
   { name: "Vũ Thanh Trúc", nickname: "Trúc", role: "Bếp phó", gender: "female" },
   { name: "Lưu Huy Hoàng", nickname: "Hoàng Sự", role: "Dược sĩ", gender: "male" },
   { name: "Lại Thị Tú Anh", nickname: "Tú Anh", role: "Thực tập sinh", gender: "female" },
-  { name: "Mai Ngọc Lan", nickname: "Lan Mắm", role: "Bếp trưởng", gender: "female" },
+  { name: "Mai Thị Ngọc Lan", nickname: "Lan Mắm", role: "Bếp trưởng", gender: "female" },
   { name: "Nguyễn Tuấn Anh", nickname: "Tuấn chó", role: "Bảo vệ", gender: "male" },
   { name: "Mai Anh Hoàng", nickname: "Hoàng Tuấn", role: "Kỹ sư IT", gender: "male" },
   { name: "Hoàng Nhật Minh", nickname: "Minh ái", role: "Con khỉ", gender: "male" },
-  { name: "Phạm Minh Thư", nickname: "Thư", role: "Nhân viên văn phòng", gender: "female" },
+  { name: "Phạm Minh Thư", nickname: "Thư Giáp", role: "Saler", gender: "female" },
   { name: "Nguyễn Vũ Dũng", nickname: "Anh Chũng", role: "Nhân viên AgriBank", gender: "male" }
 ];
